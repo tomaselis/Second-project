1ae133d1bd9db75935e274c64d46bc9e75928625
tomaselis <tsabaliauskis@hotmail.co.uk>
2019-04-30 19:45:40 +0300

    php ketvirta uzduotis

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title><?php print 'As, antradienis ir PHP';?></title>
+    <title>PHP lydes ir <?php print 'ryt' . date('Y-m-d', strtotime('+1 days'));?></title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h1> Tomas - HTML asas jau nuo <?php print date('Y') . 'metu';?></h1>
-<p>Viskas prasidejo <?php print date('m') . ' menesio,' . date('d') . ' diena!';?></p>
+<h1> Tomas - PHP su manim buvo ir  <?php print date('H', strtotime('-1 hours')) . ' valanda';?></h1>
+<p><?php print date('Y', strtotime('+ 1 years'))  . ' uz kalnu';?></p>
 </body>
 </html>
