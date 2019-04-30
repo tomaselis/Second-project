3d7179c1e34d064cfbc4949cfb5934000b0e8d16
tomaselis <tsabaliauskis@hotmail.co.uk>
2019-04-30 20:27:42 +0300

    php 6 uzduotis rand

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP lydes ir <?php print 'ryt' . date('Y-m-d', strtotime('+1 days'));?></title>
+    <title>php</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h1> Tomas - PHP su manim buvo ir  <?php print date('H', strtotime('-1 hours')) . ' valanda';?></h1>
-<p><?php print date('Y', strtotime('+ 1 years'))  . ' uz kalnu';?></p>
+<h3>PHP lydes ir <?php print date('Y-m-d', strtotime('+' . rand(0, 10) . 'years'));?></h3>
+<h1> Tomas - galbut turesiu <?php print rand(2, 5) . ' vaikus';?></h1>
+<p>D. Trumpas nebebus prezidentu <?php print rand(2021, 2031) . date('-m-d');?></p>
 </body>
 </html>
