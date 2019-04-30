7eef3bee7294037992b83365359169b813f0f2e5
tomaselis <tsabaliauskis@hotmail.co.uk>
2019-04-30 18:37:55 +0300

    antras projectas

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>As valdau HTML</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <main>
+        <h3>Kaip linksma viska rasyti online</h3>
+        <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </main>
+</body>
+</html>
