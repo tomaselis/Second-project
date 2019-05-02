e9c3cc4b1f4d7987065e5dcc4cbd0872de569c9f
tomaselis <tsabaliauskis@hotmail.co.uk>
2019-05-02 10:57:20 +0300

    php uzduotis dice

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <h3>PHP lydes ir <?php print date('Y-m-d', strtotime('+' . rand(0, 10) . 'years'));?></h3>
 <h1 style="font-size: <?php print rand(20, 80) . 'px'?>"> Tomas - galbut turesiu <?php print rand(2, 5) . ' vaikus';?></h1>
 <p style="color: rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' .  rand(0, 255);?>)">D. Trumpas nebebus prezidentu <?php print rand(2021, 2031) . date('-m-d');?></p>
+
+<div>
+    <?php echo "<img src='Photos/Dice-" .rand(1,6). ".png' alt='photo' />" ;?>
+</div>
+
 </body>
 </html>
